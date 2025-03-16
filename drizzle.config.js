e1181b99c1e3fe33e095786da26caab599ccafdd
souravkumarpanda <141444927+souravkumarpanda@users.noreply.github.com>
2025-03-16 16:03:25 +0530

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./src/utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://beat-cancer_owner:npg_D6SxFZld0stJ@ep-nameless-shadow-a1d7wqyy-pooler.ap-southeast-1.aws.neon.tech/beat-cancer?sslmode=require",
+    connectionString:
+      "postgresql://beat-cancer_owner:npg_D6SxFZld0stJ@ep-nameless-shadow-a1d7wqyy-pooler.ap-southeast-1.aws.neon.tech/beat-cancer?sslmode=require",
+  },
+};
